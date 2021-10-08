e7f9c3ebfdded2d3aa0f7741fb95c15fd2327f3e
Alex <example@gmail.com>
2021-10-08 14:53:01 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-let number = 5;
-const leftBorderWidth = 1;
+// let number = 5;
+// const leftBorderWidth = 1;
+
+const reviews = [4.5, 4.0, 5.0, 2.0, 1.0, 5.0, 3.0, 4.0, 1.0, 5.0, 4.5, 3.0, 2.5, 2.0]; 
+
+const countGroupedByReview = reviews.reduce(groupBy, {}); 
+                /* {}    4.5 */
+function groupBy (acc, review ){ 
+    const count = acc[review] || 0; 
+    return {...acc, [review]: count + 1} 
+} 
+console.log(countGroupedByReview);
