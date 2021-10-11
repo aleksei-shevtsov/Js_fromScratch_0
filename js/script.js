0aecdfc5ba07f2c67f00e78bc2c6461bdc9f03b1
Alex <example@gmail.com>
2021-10-11 13:47:05 +0300

    Switch Operator

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+"use strict";
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt(`На сколько оцените ${a}?`, ''),
-        c = prompt('Один из последних просмотренных фильмов?', ''),
-        d = prompt('На сколько оцените его?', '');
-
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB)
+if  (4 == 9) {
+    console.log('Ok!')
+}
