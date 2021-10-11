62228b750852893b436f2083556b3c63179be39d
Alex <example@gmail.com>
2021-10-11 13:17:41 +0300

    App about films started

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-// let number = 5;
-// const leftBorderWidth = 1;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const reviews = [4.5, 4.0, 5.0, 2.0, 1.0, 5.0, 3.0, 4.0, 1.0, 5.0, 4.5, 3.0, 2.5, 2.0]; 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const countGroupedByReview = reviews.reduce(groupBy, {}); 
-                /* {}    4.5 */
-function groupBy (acc, review ){ 
-    const count = acc[review] || 0; 
-    return {...acc, [review]: count + 1} 
-} 
-console.log(countGroupedByReview);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt(`На сколько оцените ${a}?`, ''),
+        c = prompt('Один из последних просмотренных фильмов?', ''),
+        d = prompt('На сколько оцените его?', '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
