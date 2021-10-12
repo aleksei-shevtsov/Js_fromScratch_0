e00643a8ad494af3dd23a50ded85f49cdc9d9fa5
Alex <example@gmail.com>
2021-10-12 18:12:59 +0300

    films marks

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-"use strict";
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-if  (4 == 9) {
-    console.log('Ok!')
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt(`На сколько оцените фильм ${a}?`, '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
 }
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Просмотрено достаточно фильмов') 
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка')
+}
+
+console.log(personalMovieDB)
